3e1b192bb0b1ba450d92fb8b25a62f25f95482f8
Maggie DeSantis <sheisntreal@gmail.com>
2026-08-27 15:14:46 -0400

    animated jellyfish with swaying tentacles

--- script.js
+++ script.js
@@ -1,47 +1,77 @@
 const canvas = document.getElementById("jellyfishCanvas");
 const ctx = canvas.getContext("2d");
 
+ctx.strokeStyle = "#675870b2";
+ctx.lineWidth = 3;
+
+
 //jellyfish dome//
+function drawBell() {
 
-ctx.beginPath();
+    ctx.beginPath();
 
-ctx.arc(
-    300, 250, 100, Math.PI, 0 );
+    ctx.arc(
+        300, 250, 100, Math.PI, 0 );
 
-ctx.lineTo(375,270);
-ctx.lineTo(350,250);
-ctx.lineTo(324,270);
-ctx.lineTo(300,250);
-ctx.lineTo(275,270);
-ctx.lineTo(250,250);
-ctx.lineTo(225,270);
-ctx.lineTo(200,250);
+    ctx.lineTo(375,270);
+    ctx.lineTo(350,250);
+    ctx.lineTo(324,270);
+    ctx.lineTo(300,250);
+    ctx.lineTo(275,270);
+    ctx.lineTo(250,250);
+    ctx.lineTo(225,270);
+    ctx.lineTo(200,250);
 
 
-ctx.closePath();
+    ctx.closePath();
 
-ctx.stroke();
+    ctx.fillStyle = "rgba(175, 144, 228, 0.2)";
+    ctx.fill();
 
+    ctx.stroke();
+}
 //jellyfish danglies//
+let time = 0;
 
-ctx.beginPath();
+function drawTentacle(x, length, direction) {
 
-ctx.moveTo(250,255);
+    let sway = Math.sin(time) * 30;
 
-ctx.bezierCurveTo(
-    220, 330,
-    270, 380,
-    250, 450
-);
+    ctx.beginPath();
 
-ctx.stroke();
+    ctx.moveTo(x, 265);
+
+    ctx.bezierCurveTo(
+        x - 20 + sway, 330,
+        x + 20 - sway, 380,
+        x + sway, length
+    );
+    
+    ctx.stroke();
+}
+
+function animate(){
+
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    drawBell();
+    drawTentacle(250, 450, 1);
+    drawTentacle(300, 475, -1);
+    drawTentacle(350, 450, 1);
+
+    time += 0.005;
+
+    requestAnimationFrame(animate);
+}
+
+animate();
 
 ctx.beginPath();
 
 ctx.moveTo(300, 260);
 
 ctx.bezierCurveTo(
-    275, 240,
+    285, 270,
     320, 390,
     300, 490
 );
